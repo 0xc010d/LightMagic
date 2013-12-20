26d434e0b5a64011679236dca87299336df49e28
0xc010d <0xc010d@gmail.com>
2013-12-20 01:13:19 +0100

    Uses executable classes only
    There's no need to inspect all possible classes in the app. We can limit them to only those which are in the executable.
    Implementation was found here: http://stackoverflow.com/questions/15149505/ios-getting-all-classes-in-a-bundle-or-loaded-app

--- LightMagic/LMCollector.m
+++ LightMagic/LMCollector.m
@@ -1,25 +1,30 @@
+#import <dlfcn.h>
+#import <mach-o/ldsyms.h>
+#import <objc/runtime.h>
 #import "LMCollector.h"
 #import "LMClass.h"
-#import <objc/runtime.h>
 
 @implementation LMCollector
 
-+ (NSSet *)classesForProtocol:(Protocol *)protocol{
++ (NSSet *)classesForProtocol:(Protocol *)protocol {
     NSMutableSet *result = [NSMutableSet set];
-    Class *classes = NULL;
-    int numClasses = objc_getClassList(NULL, 0);
-    if (numClasses > 0 ) {
-        classes = (Class *)malloc(sizeof(Class) * numClasses);
-        numClasses = objc_getClassList(classes, numClasses);
-        for (int index = 0; index < numClasses; index++) {
-            Class nextClass = classes[index];
-            if (class_conformsToProtocol(nextClass, protocol)) {
-                LMClass *clazz = [[LMClass alloc] initWithClass:nextClass];
-                [result addObject:clazz];
-            }
+
+    Dl_info info;
+    dladdr(&_mh_execute_header, &info);
+
+    uint classesCount;
+    const char **classNames = objc_copyClassNamesForImage(info.dli_fname, &classesCount);
+
+    for (uint index = 0; index < classesCount; index++) {
+        Class nextClass = objc_getClass(classNames[index]);
+        if (class_conformsToProtocol(nextClass, protocol)) {
+            LMClass *clazz = [[LMClass alloc] initWithClass:nextClass];
+            [result addObject:clazz];
         }
-        free(classes);
     }
+
+    free(classNames);
+
     return [NSSet setWithSet:result];
 }
 
