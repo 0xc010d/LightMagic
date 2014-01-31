9b28ac5ab9f2eb6473ff12bd1deb0770cd2a3b0c
0xc010d <0xc010d@gmail.com>
2014-01-31 01:43:56 +0100

    Some improvements were done

--- LightMagic/Cache/LMCache.mm
+++ LightMagic/Cache/LMCache.mm
@@ -1,18 +1,10 @@
 #include "LMCache.h"
 
 bool ClassComparator::operator()(Class a, Class b) const {
-    if (a == b) {
-        return false;
-    }
-    else if ([a isSubclassOfClass:b]) {
-        return true;
-    }
-    else if ([b isSubclassOfClass:a]) {
-        return false;
-    }
-    else {
-        return a < b;
-    }
+    if (a == b) return false;
+    else if ([a isSubclassOfClass:b]) return true;
+    else if ([b isSubclassOfClass:a]) return false;
+    else return a < b;
 }
 
 LMCache &LMCache::getInstance() {
